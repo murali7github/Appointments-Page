779c9265c599e68f7161940e511429cb8aa3c6e5
Murali <mk.golusu777@gmail.com>
2022-07-20 11:21:01 +0000

    committed first time

---
+++ src/components/AppointmentItem/index.js
@@ -0,0 +1,40 @@
+// Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {eachAppointment, onChangeStarSymble} = props
+  const {id, title, date, isStarActive} = eachAppointment
+
+  const onChangeStar = () => {
+    onChangeStarSymble(id)
+  }
+  return (
+    <li>
+      <div className="list-item">
+        <div className="title-image-container">
+          <h1 className="title-heading">{title}</h1>
+          <button type="button" className="star-button" onClick={onChangeStar}>
+            {isStarActive ? (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
+                className="image-icon"
+                alt="star"
+                testid="star"
+              />
+            ) : (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png"
+                className="image-icon"
+                alt="star"
+                testid="star"
+              />
+            )}
+          </button>
+        </div>
+        <p className="date-para">Date: {date}</p>
+      </div>
+    </li>
+  )
+}
+
+export default AppointmentItem
---
+++ src/components/Appointments/index.js
@@ -0,0 +1,171 @@
+// Write your code here
+import {Component} from 'react'
+import {format} from 'date-fns'
+import {v4 as uuidv4} from 'uuid'
+import AppointmentItem from '../AppointmentItem/index'
+import './index.css'
+
+class Appointments extends Component {
+  state = {
+    appointmentsList: [],
+    title: '',
+    date: null,
+    isStarred: false,
+    starredList: [],
+  }
+
+  onClickAddButton = event => {
+    event.preventDefault()
+    const {title, date} = this.state
+    const newAppointment = {
+      id: uuidv4(),
+      title,
+      date,
+      isStarActive: false,
+    }
+    this.setState(prevState => ({
+      appointmentsList: [...prevState.appointmentsList, newAppointment],
+      title: '',
+      date: '',
+    }))
+  }
+
+  onClickUnstarredButton = () => {
+    this.setState(prevState => ({
+      starredList: prevState.appointmentsList.filter(eachOne => {
+        if (eachOne.isStarActive === true) {
+          return eachOne
+        }
+        return null
+      }),
+      isStarred: true,
+    }))
+  }
+
+  onClickStarredButton = () => {
+    this.setState(prevState => ({
+      appointmentsList: prevState.appointmentsList,
+      isStarred: false,
+    }))
+  }
+
+  onChangeStarSymble = id => {
+    this.setState(prevState => ({
+      appointmentsList: prevState.appointmentsList.map(eachOne => {
+        if (id === eachOne.id) {
+          return {...eachOne, isStarActive: !eachOne.isStarActive}
+        }
+        return eachOne
+      }),
+      starredList: prevState.appointmentsList.filter(eachOne => {
+        if (eachOne.isStarActive === true) {
+          return eachOne
+        }
+        return null
+      }),
+    }))
+  }
+
+  onChangeTitle = event => {
+    this.setState({title: event.target.value})
+  }
+
+  onChangeDate = event => {
+    const dateFormat = format(
+      new Date(event.target.value),
+      'dd MMMM yyyy, EEEE',
+    )
+    this.setState({date: dateFormat})
+  }
+
+  render() {
+    const {appointmentsList, title, date, isStarred, starredList} = this.state
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <h1 className="heading">Add Appointment</h1>
+          <div className="image-form-container">
+            <form onSubmit={this.onClickAddButton}>
+              <div className="input-container">
+                <label htmlFor="titleInput" className="label-element">
+                  Title
+                </label>
+                <input
+                  type="search"
+                  id="titleInput"
+                  placeholder="Title"
+                  className="input-element"
+                  value={title}
+                  onChange={this.onChangeTitle}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="dateInput" className="label-element">
+                  DATE
+                </label>
+                <input
+                  type="date"
+                  id="dateInput"
+                  placeholder="dd/mm/yyy"
+                  className="input-element"
+                  value={date}
+                  onChange={this.onChangeDate}
+                />
+              </div>
+              <button type="submit" className="button">
+                Add
+              </button>
+            </form>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+              alt="appointments"
+              className="image-element"
+            />
+          </div>
+          <hr className="line-separator" />
+          <div className="appointments-container">
+            <div className="appointments-heading-container">
+              <h1 className="appointments-heading">Appointments</h1>
+              {isStarred ? (
+                <button
+                  type="button"
+                  className="starred-button"
+                  onClick={this.onClickStarredButton}
+                >
+                  Starred
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="un-starred-button"
+                  onClick={this.onClickUnstarredButton}
+                >
+                  Starred
+                </button>
+              )}
+            </div>
+            <ul className="list-container">
+              {isStarred
+                ? starredList.map(eachAppointment => (
+                    <AppointmentItem
+                      eachAppointment={eachAppointment}
+                      onChangeStarSymble={this.onChangeStarSymble}
+                      key={eachAppointment.id}
+                    />
+                  ))
+                : appointmentsList.map(eachAppointment => (
+                    <AppointmentItem
+                      eachAppointment={eachAppointment}
+                      onChangeStarSymble={this.onChangeStarSymble}
+                      key={eachAppointment.id}
+                    />
+                  ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
